11ea742d841456338f1b3c19125b529f1456bc32
Rodrigo Medeiros Lehnemann <rodrigo.medeiros@compuletra.com.br>
2020-09-15 10:25:32 -0300

    feat(Commit Inicial)

---
+++ TesteCompuletra/Program.cs
@@ -0,0 +1,19 @@
+﻿using Negocio;
+using System;
+
+namespace TesteCompuletra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var automato = new AutomatoEscritor();
+
+            // Não Mudar
+            //----------------------------------------------------
+            Executa.ExecutaAutomato(automato);
+            Console.ReadKey();
+            //----------------------------------------------------
+        }
+    }
+}
